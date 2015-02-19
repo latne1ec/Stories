cff03a37fa66e0caa40dcc47670ab7a7b0873d1a
latne1ec <latne1ec@cmich.edu>
2015-02-19 18:20:12 -0500

    Initial Commit

---
+++ Stories/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Stories
+//
+//  Created by Evan Latner on 2/19/15.
+//  Copyright (c) 2015 Evan Latner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
